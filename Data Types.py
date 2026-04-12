0a41b18d992da9fd6cfe57a519032f6768155bd3
ssukriti490-ops <ssukriti490@gmail.com>
2026-04-12 18:26:36 +0530

    Add files via upload

---
+++ Data Types.py
@@ -0,0 +1,19 @@
+name="Sukriti"
+age=12
+is_student=True
+weight=39.5
+
+print("Name:",name)
+print("Data Type ofName",type(name))
+print("Age :", age)
+print("Data Type of Age is",type(age))
+print("is_student :", is_student)
+print("Weight:",weight)
+print("Data Type of weight is",type(weight))
+
+print("\n After Type Casting....")
+age=str(age)
+print("Data Type of age is",type(age))
+weight=int(weight)
+print(weight)
+print("Data Type of weight is",type(weight))
