5a1233a078969da19e1e54d694fe33b610054eed
ssukriti490-ops <ssukriti490@gmail.com>
2026-04-26 18:39:19 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,19 @@
+def add(x,y):
+    return x + y
+
+def substract(x,y):
+    return x - y
+
+def multiply(x,y):
+    return x * y
+
+def divide(x,y):
+    return x / y
+
+num1 = int(input("Enter Number 1"))
+num2 = int( input("Enter Number 2"))
+
+print("Sum :", add(num1,num2))
+print("Difference :",substract(num1,num2))
+print("Product :", multiply(num1,num2))
+print("Ouotient :", divide(num1,num2))
