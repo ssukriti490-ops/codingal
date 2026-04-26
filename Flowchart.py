33cedebfd13ac8a96b3027fead0b5bdb712f6e0e
ssukriti490-ops <ssukriti490@gmail.com>
2026-04-26 17:07:48 +0530

    Add files via upload

---
+++ Flowchart.py
@@ -0,0 +1,12 @@
+
+print("Hello,world!")
+print("My name is Alex.")
+print("I love playing badminton")
+print("The result of 3 + 5 is:", 3 + 5)
+
+print("Welcome to the world of programming!")
+print(4)
+print("Hello Python \n")
+print("Keywords in Python are....")
+import keyword
+print(keyword.kwlist)
