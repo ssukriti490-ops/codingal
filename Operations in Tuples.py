bd51f3650d14736f5b0551bacc7870d70ab811cf
ssukriti490-ops <ssukriti490@gmail.com>
2026-05-10 18:22:59 +0530

    Add files via upload

---
+++ Operations in Tuples.py
@@ -0,0 +1,14 @@
+my_tuple = ()
+print(my_tuple)
+
+my_tuple = (1, 2, 3)
+print(my_tuple)
+
+my_tuple = (1, "Hello", 3.4)
+print(my_tuple)
+
+my_tuple = ( 'p','e','r','m','i','t')
+print(my_tuple[0])
+print(my_tuple[5])
+
+print("Sliced :", my_tuple[1:4])
