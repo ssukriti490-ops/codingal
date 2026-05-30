d48510ff438c81efe7b689626f6f6bd89174e385
ssukriti490-ops <ssukriti490@gmail.com>
2026-05-30 18:31:06 +0530

    Add files via upload

---
+++ My Bucket List.py
@@ -0,0 +1,29 @@
+file = open("bucket-list.txt", "w")
+file.write("1. Visit the Eiffel Tower\n")
+file.write("2. Learn to play the guitar\n")
+file.write("3. Code my own game\n")
+file.close()
+print("Bucket list saved to bucket-list.txtl")
+
+
+file = open("bucket-list.txt","r")
+content = file.read()
+print("\n=== My Bucket List ===")
+print(content)
+file.close()
+
+file = open("bucket-list.txt","r")
+lines = file.readlines()
+print(f"You have {len(lines)} items on your bucket list.))")
+file.close()
+
+file = open("bucket-list.txt","a")
+file.write("4. Travel to Japan\n")
+file.write("5. Run a 5k marathon\n")
+file.close()
+print("\n2 more items addedl")
+
+file = open("bucket-list.txt","r")
+print("\n === Updated Bucket List ===")
+print(file.read())
+file.close()
