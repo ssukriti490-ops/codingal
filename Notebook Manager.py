a09ba436ac0e35c57c0fb8102de2cd1d8705d9e6
ssukriti490-ops <ssukriti490@gmail.com>
2026-06-13 18:36:56 +0530

    Add files via upload

---
+++ Notebook Manager.py
@@ -0,0 +1,36 @@
+import os 
+
+print("=== Science Notes ===")
+with open("science-notes.txt","r") as f:
+    for line in f:
+        print(line.strip())
+print()
+
+print("=== World Count ===")
+with open("maths-notes.txt","r") as f:
+    for line in f:
+        words = line.split()
+        print(len(words), "words ->", line.strip())
+print()
+
+print("=== Merging Notes ===")
+if os.path.exists("all-notes.txt:"):
+    print("all-notes.txt not found - creating now")
+
+content = ""
+with open("science-notes.txt","r") as f:
+    content += "--- science-notes.txt ---\n"
+    content += f.read()+ "\n"
+with open("maths-notes.txt","r") as f:
+    content += "--- math-notes.txt ---\n"
+    content += f.read() + "\n"
+with open("all-notes.txt","w") as out:
+    out.write(content)
+print("Saved to all-notes.txt")
+print()
+
+if os.path.exists("all-notes.txt"):
+    os.remove("all-notes.txt")
+    print("all-notes.txt deleted.")
+else:
+    print("all-notes.txt does not exist.")
