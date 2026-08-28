93cd827d25812273c7212ea28d6180b5f481a745
ssukriti490-ops <ssukriti490@gmail.com>
2026-08-28 19:52:13 +0530

    Add files via upload

---
+++ Set And Zero.py
@@ -0,0 +1,13 @@
+input("Set a bit - OR truns it ON. Press Enter ")
+print("  5 =", bin(5)[2:])
+print("  5 | 2 =", 5 | 2, "  binary:", bin(5 | 2)[2:])
+input("Zero a bit - AND turns it OFF.  Press Enter ")
+print("  7 =", bin(7)[2:])
+print(" 7 & 5 =", 7 & 5, "  binary:", bin(7 & 5)[2:])
+n = int(input("Enter a number  (try 4 or 6): "))
+guess = input("Is it a power of 2? (yes/no): ")
+input("Power of 2 - only one bit is ON. Press Enter  ")
+if n > 0 and (n & (n - 1)) == 0:
+    print(" ", n, " binary:", bin(n)[2:], " power of 2 your guess:", guess)
+else:
+    print(" ", n, " binary:", bin(n)[2:], "  not power of 2 your guess:", guess)
